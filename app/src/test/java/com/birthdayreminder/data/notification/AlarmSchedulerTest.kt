4f8e75c2217dc995285be25f628640e9be5807d9
Davin07 <davin.sthomas07@gmail.com>
2026-01-08 21:31:33 +0530

    refactor(data): Update AlarmScheduler for multiple offsets and new time logic

--- app/src/test/java/com/birthdayreminder/data/notification/AlarmSchedulerTest.kt
+++ app/src/test/java/com/birthdayreminder/data/notification/AlarmSchedulerTest.kt
@@ -7,25 +7,25 @@ class AlarmSchedulerTest {
     @Test
     fun `getRequestCode generates correct code for birthday today`() {
         val requestCode = AlarmScheduler.getRequestCode(1L, 0)
-        assertEquals(10, requestCode)
+        assertEquals(100, requestCode)
     }
 
     @Test
     fun `getRequestCode generates correct code for advance reminder`() {
         val requestCode = AlarmScheduler.getRequestCode(1L, 1)
-        assertEquals(11, requestCode)
+        assertEquals(101, requestCode)
     }
 
     @Test
     fun `getRequestCode generates correct code for different birthday id`() {
         val requestCode = AlarmScheduler.getRequestCode(5L, 0)
-        assertEquals(50, requestCode)
+        assertEquals(500, requestCode)
     }
 
     @Test
     fun `getRequestCode generates correct code for 7 day advance`() {
         val requestCode = AlarmScheduler.getRequestCode(2L, 7)
-        assertEquals(27, requestCode)
+        assertEquals(207, requestCode)
     }
 
     @Test
@@ -37,6 +37,6 @@ class AlarmSchedulerTest {
     @Test
     fun `getRequestCode with large birthday id`() {
         val requestCode = AlarmScheduler.getRequestCode(100L, 0)
-        assertEquals(1000, requestCode)
+        assertEquals(10000, requestCode)
     }
 }
